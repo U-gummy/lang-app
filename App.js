ee422b18173bbb7e96cf883ba1d2e3d47e112a9b
yougmi <zoz7353@gmail.com>
2023-09-21 09:14:21 +0900

    :sparkles: icon

--- App.js
+++ App.js
@@ -44,6 +44,7 @@ const Center = styled.View`
   flex: 3;
   justify-content: center;
   align-items: center;
+  z-index: 10;
 `;
 
 const IconCard = styled(Animated.createAnimatedComponent(View))`
@@ -53,8 +54,19 @@ const IconCard = styled(Animated.createAnimatedComponent(View))`
 `;
 export default function App() {
   // Values
+  const opacity = useRef(new Animated.Value(1)).current;
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const scaleOne = position.y.interpolate({
+    inputRange: [-300, -80],
+    outputRange: [2, 1],
+    extrapolate: "clamp",
+  });
+  const scaleTwo = position.y.interpolate({
+    inputRange: [80, 300],
+    outputRange: [1, 2],
+    extrapolate: "clamp",
+  });
   // Animation
   const onPressIn = Animated.spring(scale, {
     toValue: 0.9,
@@ -68,6 +80,18 @@ export default function App() {
     toValue: 0,
     useNativeDriver: true,
   });
+  const goDropScale = Animated.timing(scale, {
+    toValue: 0,
+    duration: 100,
+    easing: Easing.linear,
+    useNativeDriver: true,
+  });
+  const goDropOpacity = Animated.timing(opacity, {
+    toValue: 0,
+    duration: 100,
+    easing: Easing.linear,
+    useNativeDriver: true,
+  });
   // Pan Responder
   const panResponder = useRef(
     PanResponder.create({
@@ -78,18 +102,37 @@ export default function App() {
       onPanResponderGrant: () => {
         onPressIn.start();
       },
-      onPanResponderRelease: () => {
-        Animated.parallel([onPressOut, goHome]).start();
+      onPanResponderRelease: (_, { dy }) => {
+        if (dy < -250 || dy > 250) {
+          Animated.sequence([
+            Animated.parallel([goDropScale, goDropOpacity]),
+            Animated.timing(position, {
+              toValue: 0,
+              duration: 300,
+              easing: Easing.linear,
+              useNativeDriver: true,
+            }),
+          ]).start(nextIcon);
+        } else {
+          Animated.parallel([onPressOut, goHome]).start();
+        }
       },
     })
   ).current;
 
   // State
-
+  const [index, setIndex] = useState(0);
+  const nextIcon = () => {
+    setIndex((prev) => prev + 1);
+    Animated.parallel([
+      Animated.spring(scale, { toValue: 1, useNativeDriver: true }),
+      Animated.spring(opacity, { toValue: 1, useNativeDriver: true }),
+    ]).start();
+  };
   return (
     <Container>
       <Edge>
-        <WordContainer>
+        <WordContainer style={{ transform: [{ scale: scaleOne }] }}>
           <Word color={GREEN}>알아</Word>
         </WordContainer>
       </Edge>
@@ -100,11 +143,11 @@ export default function App() {
             transform: [...position.getTranslateTransform(), { scale }],
           }}
         >
-          <Ionicons name="beer" color={GRAY} size={76} />
+          <Ionicons name={icons[index]} color={GRAY} size={76} />
         </IconCard>
       </Center>
       <Edge>
-        <WordContainer>
+        <WordContainer style={{ transform: [{ scale: scaleTwo }] }}>
           <Word color={RED}>몰라</Word>
         </WordContainer>
       </Edge>
